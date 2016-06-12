d4347f0c628edbdc50617b6d5d5939f1d7000812
Andrew Melvin <amelvin@gmail.com>
2016-06-12 22:45:28 +0100

    maybe githubio too

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
 
 echo "script started"
 openssl aes-256-cbc -K $encrypted_5d8775c46482_key -iv $encrypted_5d8775c46482_iv -in travis.enc -out /tmp/travis.key -d
@@ -14,7 +16,27 @@ python $TRAVIS_BUILD_DIR/travis/buildmanifest.py /tmp/package /tmp/package/manif
 #  no host checking
 #ssh -v -p 4022 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /tmp/travis.key $HOME_USER@$HOME_IP "mkdir -p ~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/latest/"
 #scp -v -P 4022 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /tmp/travis.key -rp /tmp/package/. "$HOME_USER@$HOME_IP:~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/latest/"  
-echo "repo slug = $TRAVIS_REPO_SLUG"
+//echo "repo slug = $TRAVIS_REPO_SLUG"
+
+echo "DEPLOY HOME"
  
 ssh -v -p 4022  -i /tmp/travis.key $HOME_USER@$HOME_IP "mkdir -p ~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/$1/"
 scp -v -P 4022  -i /tmp/travis.key -rp /tmp/package/. "$HOME_USER@$HOME_IP:~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/$1/"  
+
+echo "DEPLOY TO GIT"
+
+SHA=`git rev-parse --verify HEAD`
+git clone --recursive https://github.com/sticilface/sticilface.github.io /tmp/sticilface
+cd /tmp/sticilface
+mkdir -p projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/$1
+cp /tmp/package/. projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/$1/
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+ssh-add deploy_key ~\/.ssh/travisgit  
+
+git push
+# $SSH_REPO $TARGET_BRANCH
+
+
